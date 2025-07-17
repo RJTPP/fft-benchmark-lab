f3f29e833b5e411ddae4524977ac4d15c4416a79
RJTPP <97868966+RJTPP@users.noreply.github.com>
2025-07-17 09:46:22 +0700

    feat: implement inplace iterative FFT
    Accuracy is off

--- fft_core/__init__.py
+++ fft_core/__init__.py
@@ -1,3 +1,4 @@
 from .fft_base import fft_recursive, naiveDFT
 from .fft_iterative import fft_iterative
 from .fft_numba import fft_iterative_numba, fft_iterative_numba_64
+from .fft_numba_inplace import fft_iterative_numba_inplace
