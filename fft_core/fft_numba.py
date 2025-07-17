ab0cbf98a6034733646289746651efc68a30480d
RJTPP <97868966+RJTPP@users.noreply.github.com>
2025-07-17 13:11:05 +0700

    refactor: remove unused 64bits function variant

--- fft_core/fft_numba.py
+++ fft_core/fft_numba.py
@@ -50,38 +50,6 @@ def fft_iterative_numba(x: np.ndarray) -> np.ndarray:
     return x_copy
 
 
-@njit(fastmath=True, cache=True)
-def fft_iterative_numba_64(x: np.ndarray) -> np.ndarray:
-    """
-    iterative Fast Fourier Transform (FFT) on an input array using Numba for optimization.
-    Using 64-bit complex numbers for faster calculations.
-    """
-    N = x.shape[0]
-    # if N & (N - 1) != 0:
-    #     raise ValueError("Input size must be a power of 2")
-
-    indices = bit_reverse_indices(N)
-    x_copy = np.empty(N, dtype=np.complex64)
-    for i in range(N):
-        x_copy[i] = x[indices[i]]
-
-    size = 2
-    while size <= N:
-        half = size // 2
-        angle = -2j * np.pi / size
-        for start in range(0, N, size):
-            for k in range(half):
-                w = np.exp(angle * k)
-                i = start + k
-                j = i + half
-                t = w * x_copy[j]
-                x_copy[j] = x_copy[i] - t
-                x_copy[i] = x_copy[i] + t
-        size *= 2
-
-    return x_copy
-
-
 if __name__ == "__main__":
     x = np.array([1, 2, 3, 4])
     print(f"Expected: {np.fft.fft(x)}")
