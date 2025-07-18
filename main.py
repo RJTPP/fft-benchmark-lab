d4155a5ffe2a18b39d8e14edb90a1580217f14b0
RJTPP <97868966+RJTPP@users.noreply.github.com>
2025-07-18 13:52:17 +0700

    feat: add --mode flag to select test type (all, check, speed)

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ fft_functions = {
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-q", "--quiet", help="quiet mode", action="store_true")
+    parser.add_argument("-m", "--mode", help="test mode: all, check(correctness), speed", choices=["all", "check", "speed"], default="all")
     return parser.parse_args()
 
 
@@ -65,23 +66,26 @@ if __name__ == "__main__":
     if is_quiet:
         logging.getLogger().setLevel(logging.ERROR)
     
+    # Warm up
     qprint(quiet=is_quiet)
     qprint("Warming up...", quiet=is_quiet)
     test_fft_correctness(test_case.get_simple_test_cases(), verbose=False)
     
-    qprint(quiet=is_quiet)
-    qprint("Testing correctness...", quiet=is_quiet)
+    # Test Correctness
+    if args.mode in ["check", "all"]:
+        qprint(quiet=is_quiet)
+        qprint("Testing correctness...", quiet=is_quiet)
+        qprint(quiet=is_quiet)
+        test_fft_correctness(test_case.get_combined_test_cases(), verbose=is_verbose)
     
-    qprint(quiet=is_quiet)
-    test_fft_correctness(test_case.get_combined_test_cases(), verbose=is_verbose)
+    # Test Speed
+    if args.mode in ["speed", "all"]:
+        qprint(quiet=is_quiet)
+        qprint("Testing speed...", quiet=is_quiet)
+        qprint(quiet=is_quiet)
+        test_fft_speed(test_case.get_large_test_cases_extended(), verbose=is_verbose)
+        # test_fft_speed(test_case.get_large_power_of_four_test_cases(), verbose=is_verbose)
     
-    qprint(quiet=is_quiet)
-    qprint("Testing speed...", quiet=is_quiet)
-    
-    qprint(quiet=is_quiet)
-    test_fft_speed(test_case.get_large_test_cases_extended(), verbose=is_verbose)
-    # test_fft_speed(test_case.get_large_power_of_four_test_cases(), verbose=is_verbose)
-    
-    qprint(quiet=is_quiet)
+    # qprint(quiet=is_quiet)
     # print("Testing speed (2)...")
     # test_fft_speed(test_case.large_npy_one_case, verbose=True)
